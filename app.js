7e877908ce433f98733d8b547848893dc56f48af
Alan deLevie <adelevie@gmail.com>
2015-02-23 18:43:25 -0500

    Add underscore and render arbitrary tabular data into a table.

---
+++ app.js
@@ -0,0 +1,35 @@
+var data = [
+  {id: 1, name: "Alan"},
+  {id: 2, name: "Rob"}
+];
+
+// inspired by: http://jsfiddle.net/mjaric/sEwM6/
+var drawRow = function(rowData, div) {
+  var row = $("<tr />")
+  $(div).append(row);
+  var keys = _.keys(rowData);
+  _.each(keys, function(key) {
+    console.log(key);
+    row.append($("<td>" + rowData[key] + "</td>"));  
+  });
+};
+
+var drawTableHeaders = function(values, div) {
+  var thead = $("thead > tr"); 
+  _.each(values, function(value) {
+    thead.append("<th>" + value + "</th>");
+  });
+};
+
+// inspired by: http://jsfiddle.net/mjaric/sEwM6/
+var drawTable = function(data, div) {
+  drawTableHeaders(_.keys(data[0]), div);
+  for (var i = 0; i < data.length; i++) {
+    drawRow(data[i], div);
+  }
+};
+
+
+$(document).ready(function() {
+  drawTable(data, "#current_order");
+});
